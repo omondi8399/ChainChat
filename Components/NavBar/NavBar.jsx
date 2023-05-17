c3c3105bb7651c4dac6c30bdc0462d227659a82f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-17 10:26:15 +0300

    Added Components folder

---
+++ Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//INTERNAL IMPORT 
+import Style from "./NavBar.module.css"
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
